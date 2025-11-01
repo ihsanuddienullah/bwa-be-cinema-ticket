d7adbd733ab1fb06467d2a5917bb05d274903991
Muhammad Ihsanuddienullah <ihsanuddienullah@gmail.com>
2025-11-01 22:20:53 +0700

    add eslint, vscode settings, prettier rules

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+import js from '@eslint/js'
+import { globalIgnores } from 'eslint/config'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+
+export default tseslint.config([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [js.configs.recommended, tseslint.configs.recommended],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    rules: {
+      semi: ['error', 'never'],
+      quotes: ['error', 'single'],
+    },
+  },
+])
